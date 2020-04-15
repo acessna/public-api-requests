3ab1b8ecb09746e28e45713e27936d426e58fd7b
Adam Cessna <racessna@gmail.com>
2020-04-15 17:45:21 -0500

    Update fetchUserData and generateUsers methods

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,36 @@
+const userUrl = 'https://randomuser.me/api/?results=12&noinfo';
 
-const generateRandomUser = () => {
-fetch('https://randomuser.me/api/')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-  });
+
+async function fetchUserData(url){
+
+const res = await fetch(url);
+const usersJSON = await res.json();
+const users = await usersJSON.results;
+console.log(users);
+return users;
 }
 
-generateRandomUser();
+function generateUsers(data) {
+  const gallery = document.getElementById('gallery');
+  let output = '';
+
+  data.forEach(user => {
+    output = `
+    <div class="card">
+    <div class="card-img-container">
+      <img class="card-img" src="${user.picture.thumbnail}" alt="profile picture">
+  </div>
+  <div class="card-info-container">
+         <h3 id="name" class="card-name cap">${user.name.first} ${user.name.last}</h3>
+         <p class="card-text">${user.email}</p>
+         <p class="card-text cap">${user.location.city}, ${user.location.state}/p>
+     </div>
+     `;
+
+     gallery.innerHTML += output;
+  });
+
+
+};
+
+fetchUserData(userUrl);
