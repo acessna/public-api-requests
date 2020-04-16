d36661abdb4f8d76e7b2a2d06750c691148d3d54
Adam Cessna <racessna@gmail.com>
2020-04-16 10:03:56 -0500

    fixed error in calling fetchUserData and edited code comments in index.html

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,6 @@ async function fetchUserData(url){
 const res = await fetch(url);
 const usersJSON = await res.json();
 const users = await usersJSON.results;
-console.log(users);
 return users;
 }
 
@@ -17,14 +16,15 @@ function generateUsers(data) {
   data.forEach(user => {
     output = `
     <div class="card">
-    <div class="card-img-container">
-      <img class="card-img" src="${user.picture.thumbnail}" alt="profile picture">
-  </div>
-  <div class="card-info-container">
-         <h3 id="name" class="card-name cap">${user.name.first} ${user.name.last}</h3>
-         <p class="card-text">${user.email}</p>
-         <p class="card-text cap">${user.location.city}, ${user.location.state}/p>
-     </div>
+      <div class="card-img-container">
+        <img class="card-img" src="${user.picture.large}" alt="profile picture">
+      </div>
+    <div class="card-info-container">
+          <h3 id="name" class="card-name cap">${user.name.first} ${user.name.last}</h3>
+          <p class="card-text">${user.email}</p>
+          <p class="card-text cap">${user.location.city}, ${user.location.state}</p>
+      </div>
+    </div>
      `;
 
      gallery.innerHTML += output;
@@ -33,4 +33,5 @@ function generateUsers(data) {
 
 };
 
-fetchUserData(userUrl);
+fetchUserData(userUrl)
+  .then(data => generateUsers(data));
