799be9d2a123f6f80245d77389096785d25a6a25
Adam Cessna <racessna@gmail.com>
2020-04-16 12:02:36 -0500

    Worked on generating the modal window on click and exit button on click as well as importing jQuery into index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 const userUrl = 'https://randomuser.me/api/?results=12&noinfo';
+const userCards = document.getElementsByClassName('card');
+const gallery = document.getElementById('gallery');
 
 
 async function fetchUserData(url){
@@ -10,7 +12,6 @@ return users;
 }
 
 function generateUsers(data) {
-  const gallery = document.getElementById('gallery');
   let output = '';
 
   data.forEach(user => {
@@ -27,11 +28,54 @@ function generateUsers(data) {
     </div>
      `;
 
-     gallery.innerHTML += output;
+      gallery.innerHTML += output;
   });
 
 
 };
 
+function generateUserModules(data){
+
+  for (let i = 0; i < userCards.length; i++) {
+    userCards[i].addEventListener('click', () =>{
+      console.log('click'); 
+      let output = '';
+      output = `
+      <div class="modal-container">
+      <div class="modal">
+          <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+          <div class="modal-info-container">
+              <img class="modal-img" src="${data[i].picture.large}" alt="profile picture">
+              <h3 id="name" class="modal-name cap">${data[i].name.first} ${data[i].name.last}</h3>
+              <p class="modal-text">${data[i].email}</p>
+              <p class="modal-text cap">${data[i].location.city}</p>
+              <hr>
+              <p class="modal-text">${data[i].phone}</p>
+              <p class="modal-text">${data[i].location.street}, ${data[i].location.city}, ${data[i].location.state} 97204</p>
+              <p class="modal-text">Birthday: ${data[i].dob.date}</p>
+          </div>
+      </div>
+      `;
+
+      $(output).insertAfter($('.gallery'));
+
+      const closeBtn = document.getElementById('modal-close-btn');
+
+      closeBtn.addEventListener('click', () => {
+
+        $('.modal-container').remove();
+      
+      });
+    });
+  }
+
+
+};
+
+
+
 fetchUserData(userUrl)
-  .then(data => generateUsers(data));
+  .then(data => {
+    generateUsers(data); 
+    generateUserModules(data);
+  });
